504ab6c9ec01ecbe2d37c93e8e6e0a69976f3d45
mhaqnegahdar <mhaqnegahdar@gmail.com>
2024-12-25 20:51:18 +0330

    Use Prisma Accelerate

--- src/lib/prismadb.ts
+++ src/lib/prismadb.ts
@@ -1,14 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+
 // https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma: PrismaClient  ;
 };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ["query"],
-  });
+  }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
